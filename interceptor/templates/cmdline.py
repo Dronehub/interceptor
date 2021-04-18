16100669942d2124e90162f27570d0cc4a22852d
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-04-18 15:24:58 +0200

    1.0a1

--- interceptor/templates/cmdline.py
+++ interceptor/templates/cmdline.py
@@ -7,5 +7,4 @@ if __name__ == '__main__':
     tool_name = sys.argv[0].split('/')[-1]
     cfg = load_config_for(tool_name)
     args = cfg.modify(sys.argv)
-
-    os.execv(args[0], args[1:])
+    os.execv(args[0], args)
