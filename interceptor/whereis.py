45993210eff2563763d72b04965bac9449a2d688
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-04-18 14:47:50 +0200

    first commit

--- interceptor/whereis.py
+++ interceptor/whereis.py
@@ -1,23 +1,45 @@
-import os, subprocess
+import logging
+import os
+
+from satella.exceptions import ProcessFailed
+from satella.processes import call_and_return_stdout
 import sys
 from subprocess import CalledProcessError
 import typing as tp
 
-# unceremoniously taken from https://codereview.stackexchange.com/questions/190886/platform-independent-whereis-function
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+# unceremoniously taken from
+# https://codereview.stackexchange.com/questions/190886/platform-independent-whereis-function
 # and modified to better suit UNIX platforms
 def whereis(app: str) -> tp.List[str]:
+    result = None
+    print('Gunning for %s' % (repr(app), ))
     try:
-        result = subprocess.check_output(["whereis", app])
-
-    except CalledProcessError:
+        args = ['whereis', app]
+        print(args)
+        result = call_and_return_stdout(args, shell=True,
+                                        expected_return_code=0,
+                                        encoding='utf-8')
+    except ProcessFailed:
         print('whereis not found, aborting')
         sys.exit(1)
 
     if result is None:
         return []
     else:
-        result = result.decode().replace("\r", "").split("\n")
-        result = list(filter(lambda x: len(x), result))
+        result = result.replace("\r", "").split("\n")
+        results = []
+        for line in result:
+            line = line.strip()
+            if not line:
+                continue
+            if ':' in line:
+                results.append(line.split(':', 1)[1])
+            else:
+                results.append(line)
         return result
 
 
@@ -28,4 +50,6 @@ def filter_whereis(app: str) -> str:
     for path in whereis(app):
         if os.access(path, os. X_OK):
             return path
+    print('%s not found, aborting' % (app, ))
+    sys.exit(1)
 
