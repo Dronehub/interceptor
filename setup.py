c333e12c03ac04a4b381f1e8058c093fb46a53bf
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-05-14 17:28:45 +0200

    fixed intercept check code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='interceptor',
-    version='2.0rc18',
+    version='2.0rc19',
     author='Piotr Maślanka',
     install_requires=['satella'],
     package_data={'interceptor': ['templates/cmdline.py',
