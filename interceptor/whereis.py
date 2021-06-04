a9d0f8df790c009fcd4f0769179140bbea2717f5
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-06-04 18:33:01 +0200

    moved whereis to satella

--- interceptor/whereis.py
+++ interceptor/whereis.py
@@ -1,42 +1,12 @@
 import logging
 import os
 
-from satella.exceptions import ProcessFailed
-from satella.processes import call_and_return_stdout
+from satella.os import whereis
 import sys
 import typing as tp
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
-
-
-# unceremoniously taken from
-# https://codereview.stackexchange.com/questions/190886/platform-independent-whereis-function
-# and modified to better suit UNIX platforms
-def whereis(app: str) -> tp.List[str]:
-    result = None
-    try:
-        args = ['whereis', app]
-        result = call_and_return_stdout(args,
-                                        expected_return_code=0,
-                                        encoding='utf-8')
-    except ProcessFailed:
-        print('whereis not found, aborting')
-        sys.exit(1)
-
-    if result is None:
-        return []
-    else:
-        result = result.replace("\r", "").split("\n")
-        results = []
-        for line in result:
-            line = line.strip()
-            if not line:
-                continue
-
-            if ':' in line:
-                results = line.split(':', 1)[1].strip().split(' ')
-        return results
 
 
 def filter_whereis(app: str, abort_on_failure=True) -> tp.Iterator[str]:
@@ -47,9 +17,8 @@ def filter_whereis(app: str, abort_on_failure=True) -> tp.Iterator[str]:
     for path in whereis(app):
         if os.path.isdir(path):
             continue
-        if os.access(path, os.X_OK):
-            found = True
-            yield path
+        found = True
+        yield path
 
     if not found:
         if not abort_on_failure:
--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
 from setuptools import setup
 
-setup(
-    install_requires=['satella'],
-    package_data={'interceptor': ['templates/cmdline.py',
-                                  'templates/config']},
-    packages=[
-        'interceptor',
-    ],
-    entry_points={
-        'console_scripts': [
-            'intercept = interceptor.run:run'
-        ]
-    },
-)
+setup()
+
